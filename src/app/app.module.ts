afe704852709902835203a8c87fa09b831652f9f
Miguel Bohorquez <mbohorquez65@gmail.com>
2020-08-24 09:49:01 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { TodoModule } from "./todos/todo.module";
+import { FooterComponent } from './footer/footer.component';
+import { todoReducer } from "./todos/todo.reducer";
+
+//NgRx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({ todos: todoReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,16 @@
+import { createReducer, on } from '@ngrx/store';
+import {crear} from './todo.actions';
+import { Todo } from "./models/todo.model";
+ 
+export const estadoInicial: Todo[] = [
+  new Todo('salvar el mundo')
+];
+ 
+const _todoReducer = createReducer(
+  estadoInicial,
+  on(crear, (state, {texto}) => [...state, new Todo(texto)]),
+);
+ 
+export function todoReducer(state, action) {
+  return _todoReducer(state, action);
+}
